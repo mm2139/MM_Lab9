721f6fae578dad2d09514ef6173ed63b0e5cdd4e
mm2139 <maalin.622139@swl.k12.oh.us>
2024-11-11 08:55:14 -0500

    not fin

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
-public class Main {
+import java.util.Scanner;
+import java.util.Random;
+
+
+public class Lab9 {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Scanner scan = new Scanner(System.in);
+        Random rand = new Random();
+
+        Tasks.Task1(rand);
+
     }
 }
---
+++ src/Tasks.java
@@ -0,0 +1,16 @@
+import java.util.Random;
+
+public class Tasks {
+
+    public static void Task1(Random rand) {
+
+        int numList[] = new int [100];
+
+        for (int i = 0; i<100; i++) {
+
+        }
+
+    }
+
+
+}
