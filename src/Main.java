e7b3f2927dbeeec03b86eea81435ecd7215ccb43
mm2139 <maalin.622139@swl.k12.oh.us>
2024-11-12 08:51:15 -0500

    not fin

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 import java.util.Random;
 
 
-public class Lab9 {
+public class Main {
     public static void main(String[] args) {
 
         Scanner scan = new Scanner(System.in);
--- src/Tasks.java
+++ src/Tasks.java
@@ -7,10 +7,34 @@ public class Tasks {
         int numList[] = new int [100];
 
         for (int i = 0; i<100; i++) {
-
+            numList[i]=rand.nextInt(101);
         }
 
-    }
+        int numListIndex = 0;
+        int sum = 0;
+        int max = 0;
+        int min = 100;
 
+        for (int number : numList) {
+            numListIndex++;
+            System.out.printf("%-9s", "Value " + numListIndex);
+            System.out.println(" | " + number + "\n");
+            sum += number;
+            if (number>max) {
+                max = number;
+            }
+            if (number<min) {
+                min = number;
+            }
+        }
+
+        int average = sum/100;
+        System.out.println("Sum of all values: " + sum);
+        System.out.println("Average of all values: " + average);
+        System.out.println("Max value: " + max);
+        System.out.println("Min value: " + min);
+
+
+    }
 
 }
