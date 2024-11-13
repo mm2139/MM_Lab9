be749dbea379f32096807e0435cc7912c8db3f45
mm2139 <maalin.622139@swl.k12.oh.us>
2024-11-13 08:47:44 -0500

    finished everything but ec

--- src/Main.java
+++ src/Main.java
@@ -8,7 +8,62 @@ public class Main {
         Scanner scan = new Scanner(System.in);
         Random rand = new Random();
 
-        Tasks.Task1(rand);
+        int valuesArray[] = new int[100];
+
+        for (int i = 0; i < 100; i++) {
+            valuesArray[i] = rand.nextInt(101);
+        }
+
+        int numListIndex = 0;
+        int sum = 0;
+        int max = 0;
+        int min = 100;
+
+        for (int number : valuesArray) {
+            numListIndex++;
+            System.out.printf("%-9s", "Value " + numListIndex);
+            System.out.println(" | " + number + "\n");
+            sum += number;
+            if (number > max) {
+                max = number;
+            }
+            if (number < min) {
+                min = number;
+            }
+        }
+
+        int average = sum / 100;
+        System.out.println("Sum of all values: " + sum);
+        System.out.println("Average of all values: " + average);
+        System.out.println("Max value: " + max);
+        System.out.println("Min value: " + min + "\n");
+
+
+        int userNum = InputHelper.getRangedInt(scan, "Please input an integer within the following range", 1, 100);
+        int checker = 0;
+        for (int i = 0; i<valuesArray.length; i++) {
+            if (valuesArray[i]==userNum) {
+                checker++;
+            }
+        }
+        System.out.println("The number " + userNum + " appeared in the list of values " + checker + " times.\n");
+
+        checker = -2;
+
+        userNum = InputHelper.getRangedInt(scan, "Please input another integer within the following range", 1, 100);
+        for (int i = 0; i<valuesArray.length; i++) {
+            if (valuesArray[i]==userNum) {
+                checker = i;
+                break;
+            }
+        }
+
+        if (checker == -1) {
+            System.out.println("The number " + userNum + " does not appear in the list of valuees.");
+        } else {
+            System.out.println("The value Value " + (checker+1) + " appears in the array at index " + checker + ".");
+        }
 
     }
+
 }
